df01c303809b7a0f0f168b65e4fc2424541116b0
daladim <daladim@users.noreply.github.com>
2022-09-16 14:51:41 +0200

    SchemaLocator has interior mutability
    This will make it easier to use across various threads

--- src/provider.rs
+++ src/provider.rs
@@ -272,7 +272,7 @@ pub mod kernel_providers {
         KernelProvider::new(kernel_guids::ALPC_GUID, kernel_flags::EVENT_TRACE_FLAG_ALPC);
 }
 
-type EtwCallback = Box<dyn FnMut(&EventRecord, &mut SchemaLocator) + Send + Sync + 'static>;
+type EtwCallback = Box<dyn FnMut(&EventRecord, &SchemaLocator) + Send + Sync + 'static>;
 
 /// Main Provider structure
 pub struct Provider {
@@ -448,7 +448,7 @@ impl Provider {
     /// # use ferrisetw::provider::Provider;
     /// # use ferrisetw::native::etw_types::EventRecord;
     /// # use ferrisetw::schema_locator::SchemaLocator;
-    /// Provider::new().add_callback(|record: &EventRecord, schema_locator: &mut SchemaLocator| {
+    /// Provider::new().add_callback(|record: &EventRecord, schema_locator: &SchemaLocator| {
     ///     // Handle Event
     /// });
     /// ```
@@ -456,7 +456,7 @@ impl Provider {
     /// [SchemaLocator]: crate::schema_locator::SchemaLocator
     pub fn add_callback<T>(self, callback: T) -> Self
     where
-        T: FnMut(&EventRecord, &mut SchemaLocator) + Send + Sync + 'static,
+        T: FnMut(&EventRecord, &SchemaLocator) + Send + Sync + 'static,
     {
         if let Ok(mut callbacks) = self.callbacks.write() {
             callbacks.push(Box::new(callback));
@@ -510,7 +510,7 @@ impl Provider {
         &self.filters
     }
 
-    pub(crate) fn on_event(&self, record: &EventRecord, locator: &mut SchemaLocator) {
+    pub(crate) fn on_event(&self, record: &EventRecord, locator: &SchemaLocator) {
         // Has to be mutable because the SchemaLocator will be mutated when locating the schema
         // within the cb creating a clone of the whole SchemaLocator HashMap doesn't
         // sound like a plan still needs to think more about this thou...
